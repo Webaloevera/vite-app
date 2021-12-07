0391bdaae24c028523525420b13c7b123dad4c2f
Dmitriy Cherenkov <dimacherenkov97@gmail.com>
2021-12-07 15:22:56 +0200

    changed

--- src/components/ImagePreloader/ImagePreloader.jsx
+++ src/components/ImagePreloader/ImagePreloader.jsx
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from 'react';
 import Skeleton from 'react-loading-skeleton';
 import PropTypes from 'prop-types';
 import 'react-loading-skeleton/dist/skeleton.css'
-import './imagePreloader.css';
 
 
 const IMG_STATUS = {
